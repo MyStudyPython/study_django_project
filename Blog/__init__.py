121f0ee9982b894768e1537d17b24d4343d90554
lyxlwz <1850115720@qq.com>
2023-06-09 10:56:27 +0800

    🎉 init

---
+++ Blog/__init__.py
@@ -0,0 +1,3 @@
+# 引入pymysql 库
+import pymysql 
+pymysql.install_as_MySQLdb()
--- Blog/settings.py
+++ Blog/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+ 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ 'default': {
+    'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+    'NAME': 'Blog', # 数据库名称
+    'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1
+    'PORT': 3306, # 端口
+    'USER': 'root', # 数据库用户名
+    'PASSWORD': '123456', # 数据库密码
+ }
 }
 
 
