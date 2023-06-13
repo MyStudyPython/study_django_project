16afc18eecaea80ab4550e152404b74c9dba33fe
lyxlwz <1850115720@qq.com>
2023-06-13 09:44:41 +0800

    🤔 update

--- application/models.py
+++ application/models.py
@@ -11,16 +11,20 @@ from django.utils import timezone
 class Article(models.Model):
     # 文章id,主键
     id = models.AutoField(primary_key=True)
-    # 文章作者，用于指定数据删除的方式
-    author = models.CharField(max_length=100)
+
+    # # 文章作者，用于指定数据删除的方式
+    # author = models.CharField(max_length=100)
+    # 文章作者。修改为User的外键，参数 on_delete 用于指定数据删除的方式
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # 文章标题，model.CharField 为字符串字段，用于保存较短的字符串，比如标题
-    title = models.CharField(max_length=100)
+    title = models.CharField("标题", max_length=100)
 
     # 文章正文，保存大量文本使用 TextField
-    body = models.TextField()
+    body = models.TextField("文章正文")
 
     # 文章创建时间，参数default = timezone.now 指定其在创建数据时将默认写入当前的时间
     createTime = models.DateTimeField(default=timezone.now)
 
-    # 文章更新时间，参数auto_now = True
+    # 文章更新时间，参数auto_now = True 指定每次数据更新时自动写入当前时间
     updateTime = models.DateTimeField(auto_now=True)
